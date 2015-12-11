bd8e287b71b8763c61e99c210dfae82e8e2a3dfe
Arto Heino <Artsi@10.112.217.160>
2015-12-11 14:36:46 +0200

    Login.js fixed

--- NetBeansProjects/image-sharing-service/web/js/login.js
+++ NetBeansProjects/image-sharing-service/web/js/login.js
@@ -6,82 +6,78 @@
 
 var printed = false;
 function login() {
-    $("#login").submit(function(e){
-    var postData = $(this).serialize();
-    var formURL = $(this).attr("action");
-    var logged = "no";
-    
-    $.ajax(
-    {
-        url : formURL,
-        type: "POST",
-        data : postData,
-        success:function(data)
-        {
-            
-            parsedData = null;
-            parsedData = JSON.parse(data);
-            
-           
-            if (parsedData[0]=="no"){
-               
-                $('#username').val('');
-                $('#password').val('');
-                if (!printed){
-                    $( "#after" ).append( "<p>Wrong username or password</p>" );
-                    printed = true;
-                }
-            }else{
-            
-            sessionStorage.setItem('user', parsedData[0]);
-            sessionStorage.setItem('role', parsedData[1]);
-            sessionStorage.setItem('userId', parsedData[2]);
-            
-            logged = "yes";
-            window.location.replace('http://192.168.56.1:8080/image-sharing-service/');
-            //alert("Kirjautuneena: "+ parsedData);
-            }
-            
-         sessionStorage.setItem('loggedIn', logged);
-         
-        }
-    })
-    e.preventDefault(); //STOP default action
-});
-$("#login").submit();
-};
-   
-    
+    $("#login").submit(function (e) {
+        var postData = $(this).serialize();
+        var formURL = $(this).attr("action");
+        var logged = "no";
+
+        $.ajax(
+                {
+                    url: formURL,
+                    type: "POST",
+                    data: postData,
+                    success: function (data)
+                    {
+
+                        parsedData = null;
+                        parsedData = JSON.parse(data);
+
+
+                        if (parsedData[0] === "no") {
+
+                            $('#username').val('');
+                            $('#password').val('');
+                            if (!printed) {
+                                $("#after").append("<p>Wrong username or password</p>");
+                                printed = true;
+                            }
+                        } else {
+
+                            sessionStorage.setItem('user', parsedData[0]);
+                            sessionStorage.setItem('role', parsedData[1]);
+                            sessionStorage.setItem('userId', parsedData[2]);
+
+                            logged = "yes";
+                            window.location.replace('index.html');
+                            //alert("Kirjautuneena: "+ parsedData);
+                        }
+
+                        sessionStorage.setItem('loggedIn', logged);
+
+                    }
+                });
+        e.preventDefault(); //STOP default action
+    });
+    $("#login").submit();
+}
+;
+
+
 
 function logout() {
-    sessionStorage.setItem('loggedIn', 'no')
-     //$("#logout").load();
-     //$("#login1").load();
-     //$( "#user" ).load();
+    sessionStorage.setItem('loggedIn', 'no');
+    //$("#logout").load();
+    //$("#login1").load();
+    //$( "#user" ).load();
 }
 function onLoad() {
-        
-            var status = sessionStorage.getItem('loggedIn');
-            if (!status){
-                sessionStorage.setItem('loggedIn', 'no');
-                status = sessionStorage.getItem('loggedIn');
-            }
-            
-            if (status=="no"){//out
-                $( "#logout" ).hide();
-                $( "#user" ).empty();
-                
-            }
-            else{//in
-                $( "#login1" ).hide();
-                $( "#user" ).empty();
-                $( "#user" ).append( sessionStorage.getItem('user') );
-                
-            }
-            
-        }
-   
 
+    var status = sessionStorage.getItem('loggedIn');
+    if (!status) {
+        sessionStorage.setItem('loggedIn', 'no');
+        status = sessionStorage.getItem('loggedIn');
+    }
 
+    if (status === "no") {//out
+        $("#logout").hide();
+        $("#user").empty();
 
+    }
+    else {//in
+        $("#login1").hide();
+        $("#user").empty();
+        $("#user").append(sessionStorage.getItem('user'));
 
+    }
+
+}
